8a7e0c33829e5e94b8a0831edc8c7e61a094cddc
RomainCorb <45872741+RomainCorb@users.noreply.github.com>
2019-01-07 10:01:44 +0100

    Add files via upload

--- src/main/java/cc_ski_track/ski_tracker/Examples/Arthur.java
+++ src/main/java/cc_ski_track/ski_tracker/Examples/Arthur.java
@@ -1,13 +1,13 @@
 package cc_ski_track.ski_tracker.Examples;
 
 import android.opengl.GLES20;
-
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 import java.nio.ShortBuffer;
 
 import cc_ski_track.ski_tracker.MyGLRenderer;
+import cc_ski_track.ski_tracker.TextureHelper;
 
 public class Arthur {
     private final int mProgram;
@@ -16,35 +16,57 @@ public class Arthur {
     private int mPositionHandle;
     private int mColorHandle;
 
+    /** TEST */
+    /* Store our model data in a float buffer. */
+    private final FloatBuffer textureBuffer;
+    private int mTextureCoordinateHandle;
+    /** FIN TEST */
+
+    static float squareCoords[] = {
+            -0.5f,  0.5f, 0.0f,   // top left
+            -0.5f, -0.5f, 0.0f,   // bottom left
+             0.5f, -0.5f, 0.0f,   // bottom right
+             0.5f,  0.5f, 0.0f    // top right
+    };
+
+    final float texture[] = {
+            1,1, 0,1, 1,0, 0,0
+    };
+
+    private short drawOrder[] = {
+            0, 1, 2, 0, 2, 3 // order to draw vertices
+    };
+
+    float color[] = {
+            0.63671875f, 0.76953125f, 0.22265625f, 1.0f /**test angle fct couleur?*/
+    };
 
     private final String vertexShaderCode =
-            "uniform mat4 uMVPMatrix;" +
+                    "uniform mat4 uMVPMatrix;" +
                     "attribute vec4 vPosition;" +
+                    "varying vec2 vCoord; \n" +
                     "void main() {" +
                     "  gl_Position = uMVPMatrix * vPosition;" +
+                    "  vCoord = aCoord; \n" +
                     "}";
 
     private final String fragmentShaderCode =
-            "precision mediump float;" +
-                    "uniform vec4 vColor;" +
+                    "precision mediump float;" +
+                    //"uniform vec4 vColor;" +
+                    "varying vec2 vCoord; \n" +
+                    "uniform sampler2D uSampler; \n" +
                     "void main() {" +
-                    "  gl_FragColor = vColor;" +
+                    "  gl_FragColor = texture2D(uSampler, vCoord); \n" +
                     "}";
     // Use to access and set the view transformation
     private int mMVPMatrixHandle;
 
     static final int COORDS_PER_VERTEX = 3;
-    static float squareCoords[] = {
-            -0.5f,  0.5f, 0.0f,   // top left
-            -0.5f, -0.5f, 0.0f,   // bottom left
-            0.5f, -0.5f, 0.0f,   // bottom right
-            0.5f,  0.5f, 0.0f }; // top right
-    
-    private short drawOrder[] = { 0, 1, 2, 0, 2, 3 }; // order to draw vertices
+
     private final int vertexStride = COORDS_PER_VERTEX * 4; // 4 bytes per vertex
     // number of coordinates per vertex in this array
     // Set color with red, green, blue and alpha (opacity) values
-    float color[] = { 0.63671875f, 0.76953125f, 0.22265625f, 1.0f }; //test avec couleur?
+
 
     public Arthur() {
         // initialize vertex byte buffer for shape coordinates
@@ -61,6 +83,13 @@ public class Arthur {
         drawListBuffer.put(drawOrder);
         drawListBuffer.position(0);
 
+        //initialize texture byte buffer for the texture list
+        ByteBuffer tb = ByteBuffer.allocateDirect(texture.length * 4);
+        tb.order(ByteOrder.nativeOrder());
+        textureBuffer = tb.asFloatBuffer();
+        textureBuffer.put(squareCoords);
+        textureBuffer.position(0);
+
         int vertexShader = MyGLRenderer.loadShader(GLES20.GL_VERTEX_SHADER,vertexShaderCode);
         int fragmentShader = MyGLRenderer.loadShader(GLES20.GL_FRAGMENT_SHADER,fragmentShaderCode);
 
@@ -75,11 +104,11 @@ public class Arthur {
     }
 
 
-
     public void draw(float[] mvpMatrix) {
         // Add program to OpenGL ES environment
         GLES20.glUseProgram(mProgram);
         // get handle to vertex shader's vPosition member
+
         mPositionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
         // Enable a handle to the triangle vertices
         GLES20.glEnableVertexAttribArray(mPositionHandle);
@@ -87,12 +116,18 @@ public class Arthur {
         GLES20.glVertexAttribPointer(mPositionHandle, COORDS_PER_VERTEX,
                 GLES20.GL_FLOAT, false,
                 vertexStride, vertexBuffer);
-        // get handle to fragment shader's vColor member
+
+        // Pass in the texture coordinate information
+        textureBuffer.position(0);
+        mTextureCoordinateHandle = GLES20.glGetUniformLocation(mProgram,"vCoord");
+        GLES20.glVertexAttribPointer(mTextureCoordinateHandle, mTextureCoordinateDataSize, GLES20.GL_FLOAT, false,
+                0, mTextureCoordinates);
+        GLES20.glEnableVertexAttribArray(mTextureCoordinateHandle);
+
+        /**COULEUR // get handle to fragment shader's vColor member
         mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
         // Set color for drawing the triangle
-        GLES20.glUniform4fv(mColorHandle, 1, color, 0);
-
-
+        GLES20.glUniform4fv(mColorHandle, 1, color, 0);*/
 
         // get handle to shape's transformation matrix
         mMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
