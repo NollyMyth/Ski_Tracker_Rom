bc2394de84f84a8f267bc0fe749f0849a7c2f72e
RomainCorb <45872741+RomainCorb@users.noreply.github.com>
2019-01-15 09:22:04 +0100

    Add files via upload

--- src/main/java/cc_ski_track/ski_tracker/Examples/Geometry/Vec3.java
+++ src/main/java/cc_ski_track/ski_tracker/Examples/Geometry/Vec3.java
@@ -14,11 +14,15 @@ public class Vec3 {
     }
 
     public Vec3 sum(Vec3 v1){
-        return new Vec3 (x + v1.x, y + v1.y, z + v1.z);
+          return new Vec3 (x + v1.x, y + v1.y, z + v1.z);
+    }
+
+    public Vec3 div(float i){
+        return new Vec3 (x/i, y/i, z/i);
     }
 
     public Vec3 cross(Vec3 v1){
-        return new Vec3 (x * v1.y - v1.x * y, v1.z * v1.x - x * v1.z, x * v1.y - y*v1.x);
+        return new Vec3 (y * v1.z - v1.y * z, z * v1.x - x * v1.z, x * v1.y - y*v1.x);
     }
 
     public void normalize(){
